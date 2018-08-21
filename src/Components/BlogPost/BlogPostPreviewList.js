87fa753198bdec308c182e48152c15d5f898166b
vpalermo <vpalermo@tabsrl.com>
2018-08-21 13:14:46 +0200

    modificato widget blog overview

--- src/Components/BlogPost/BlogPostPreviewList.js
+++ src/Components/BlogPost/BlogPostPreviewList.js
@@ -8,103 +8,107 @@ import isImage from '../../utils/isImage';
 import { textExtractFromObj } from '../../utils/textExtract';
 
 const BlogPostPreviewList = Scrivito.connect(({ maxItems, author, tag }) => {
-  let blogPosts = Scrivito.getClass('BlogPost').all().order('publishedAt', 'asc');
-  if (author) {
-    blogPosts = blogPosts.and('author', 'refersTo', author);
-  }
-  if (tag) {
-    blogPosts = blogPosts.and('tags', 'equals', tag);
-  }
+    let blogPosts = Scrivito.getClass('BlogPost').all().order('publishedAt', 'asc');
+    if (author) {
+        blogPosts = blogPosts.and('author', 'refersTo', author);
+    }
+    if (tag) {
+        blogPosts = blogPosts.and('tags', 'equals', tag);
+    }
 
-  let posts;
-  if (maxItems) {
-    posts = blogPosts.take(maxItems);
-  } else {
-    posts = [...blogPosts];
-  }
+    let posts;
+    if (maxItems) {
+        posts = blogPosts.take(maxItems);
+    } else {
+        posts = [...blogPosts];
+    }
 
-  if (!posts.length) {
+    if (!posts.length) {
+        return (
+                <InPlaceEditingPlaceholder center={ true }>
+                    There are no blog posts. Create one using the page menu.
+                </InPlaceEditingPlaceholder>
+                );
+    }
+
+    const months = groupBy(posts, post => {
+        const publishedAt = post.get('publishedAt');
+        return publishedAt && formatDate(publishedAt, 'mmmm yyyy');
+    });
+
     return (
-      <InPlaceEditingPlaceholder center={ true }>
-        There are no blog posts. Create one using the page menu.
-      </InPlaceEditingPlaceholder>
-    );
-  }
-
-  const months = groupBy(posts, post => {
-    const publishedAt = post.get('publishedAt');
-    return publishedAt && formatDate(publishedAt, 'mmmm yyyy');
-  });
-
-  return (
-    <React.Fragment>
-      {
-        Object.entries(months).map(([month, monthPosts]) =>
-          <React.Fragment key={ `month: ${month}` }>
-            
-            <PostsTimeline posts={ monthPosts } />
-          </React.Fragment>
-        )
-      }
-    </React.Fragment>
-  );
+            <React.Fragment>
+                {
+                    Object.entries(months).map(([month, monthPosts]) =>
+                        <React.Fragment key={ `month: ${month}` }>
+                    
+                            <PostsTimeline posts={ monthPosts } />
+                        </React.Fragment>
+                    )
+                }
+            </React.Fragment>
+            );
 });
 
 const MonthHeadline = Scrivito.connect(({ date }) => {
-  if (!date) { return null; }
+    if (!date) {
+        return null;
+    }
 
-  return (
-    <ul className="timeline">
-      <li className="timeline-divider">
-        <time dateTime={ formatDate(date, 'mm-yyyy') }>
-          { formatDate(date, 'mmmm yyyy') }
-        </time>
-      </li>
-    </ul>
-  );
+    return (
+            <ul className="timeline">
+                <li className="timeline-divider">
+                    <time dateTime={ formatDate(date, 'mm-yyyy') }>
+                        { formatDate(date, 'mmmm yyyy') }
+                    </time>
+                </li>
+            </ul>
+            );
 });
 
 const PostsTimeline = Scrivito.connect(({ posts }) =>
-  <ul className="timeline">
-    { posts.map(post => <BlogPostPreview key={ post.id() } post={ post } />) }
-  </ul>
+    <ul className="timeline">
+        { posts.map(post => <BlogPostPreview key={ post.id() } post={ post } />) }
+    </ul>
 );
 
 const BlogPostPreview = Scrivito.connect(({ post }) => {
-  return (
-    <li>
-      <BlogPostDate post={ post } />
-      <div className="timeline-panel">
-        <div className="timeline-body">
-          <BlogPostTitleImage post={ post } />
-          <h3>
-              { post.get('title') }
-          </h3>
-          <h4>{ post.get('subtitle') }</h4>
-          <p>{ truncate(textExtractFromObj(post), { length: 300, separator: /,? +/ }) }</p>
-        </div>
-        <div className="timeline-footer">
+    return (
+            <li>
+            <BlogPostDate post={ post } />
+            <div className="timeline-panel">
+                <div className="timeline-body">
+                    <BlogPostTitleImage post={ post } />
+                    <h3>
+                        { post.get('title') }
+                    </h3>
+                    <h4>{ post.get('subtitle') }</h4>
+                    <p>{ truncate(textExtractFromObj(post), {length: 300, separator: /,? +/ }) }</p>
+                </div>
+                <div className="timeline-footer">
+            
+                </div>
+            </div>
+            </li>
+                );
+    });
 
-        </div>
-      </div>
-    </li>
-  );
-});
+    const BlogPostTitleImage = Scrivito.connect(({ post }) => {
+        const titleImage = post.get('titleImage');
+        console.log('titleImage', post);
+        if (!isImage(titleImage)) {
+            return null;
+        }
 
-const BlogPostTitleImage = Scrivito.connect(({ post }) => {
-  const titleImage = post.get('titleImage');
-  console.log('titleImage', post);
-  if (!isImage(titleImage)) { return null; }
+        return (
+                <a href="https://cdn0.scrvt.com/6929351aa2f94e8ebc042ac9cf43c635/e9804c6c28780903/4e33fbcdf682/v/05c5b0065bc9/locandina_fds.jpg" target="_blank">
+                    <Scrivito.ImageTag
+                        content={ titleImage }
+                        className="img-responsive"
+                        alt={ titleImage.get('alternativeText') }
+                        />
+                </a>
+                );
+    });
 
-  return (
-  
-      <Scrivito.ImageTag
-        content={ titleImage }
-        className="img-responsive"
-        alt={ titleImage.get('alternativeText') }
-      />
-   
-  );
-});
-
-export default BlogPostPreviewList;
+    export default BlogPostPreviewList;
