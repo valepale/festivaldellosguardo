3295ecdad2f4422381954d794ab5ead2bb85dfc9
valeria <valeetpale@gmail.com>
2018-09-10 22:08:36 +0200

    component fileUpload in contact

--- src/Components/FileUpload.js
+++ src/Components/FileUpload.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as Scrivito from 'scrivito';
+import axios, { post } from 'axios';
 
 
 class FileUpload extends React.Component {
@@ -7,7 +8,8 @@ class FileUpload extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            file: null
+            file: null,
+            target: null
         }
         this.onFormSubmit = this.onFormSubmit.bind(this)
         this.onChange = this.onChange.bind(this)
@@ -15,17 +17,17 @@ class FileUpload extends React.Component {
     }
     onFormSubmit(e) {
         e.preventDefault() // Stop form submit
-        this.fileUpload(this.state.file).then((response) => {
+        this.fileUpload(this.state).then((response) => {
             console.log(response.data);
         })
     }
     onChange(e) {
-        this.setState({file: e.target.files[0]})
+        this.setState({file: e.target.files[0], target: e.target})
     }
-    fileUpload(file) {
-        const url = 'http://example.com/file-upload';
+    fileUpload(state) {
+        const url = 'https://www.festivaldellosguardo.it';
         const formData = new FormData();
-        formData.append('file', file)
+        formData.append('file', state.file)
         const config = {
             headers: {
                 'content-type': 'multipart/form-data'
@@ -34,14 +36,94 @@ class FileUpload extends React.Component {
         return  post(url, formData, config)
     }
 
-    render() {
-        const instafeedTarget = 'instafeed';
-        return (
-                <div>
-                    <input type="file" name="file" onChange={this.onChange} />
-                </div>
-                )
-    }
+
+ render() {
+  return (
+    <div className="row card-white-transparent">
+        <form className="row" onSubmit={this.onFormSubmit}>
+        <input
+          type="hidden"
+          name="form-name"
+          value="contact"
+        />
+        <div className="d-none">
+          <label>Don’t fill this out: <input name="bot-field" /></label>
+        </div>
+        <div className="col-sm-6">
+          <div className="form-group">
+            <label htmlFor="contactName">Your name</label>
+            <input
+              className="form-control form-control-lg"
+              id="contactName"
+              name="contactName"
+              placeholder="Name"
+              type="text"
+              required
+            />
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="form-group">
+            <label htmlFor="contactFamilyName">Family name</label>
+            <input
+              className="form-control form-control-lg"
+              id="contactFamilyName"
+              name="contactFamilyName"
+              placeholder="Family name"
+              type="text"
+              required
+            />
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="form-group">
+            <label htmlFor="contactEmail">Email address</label>
+            <input
+              className="form-control form-control-lg"
+              id="contactEmail"
+              name="contactEmail"
+              placeholder="Email"
+              type="email"
+              required
+            />
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="form-group">
+            <label htmlFor="contactCompany">Company (optional)</label>
+            <input
+              className="form-control form-control-lg"
+              id="contactCompany"
+              name="contactCompany"
+              placeholder="Company"
+              type="text"
+            />
+          </div>
+        </div>
+        <div className="col-sm-12">
+          <div className="form-group">
+            <label htmlFor="contactMessage">Message</label>
+            <textarea
+              className="form-control form-control-lg"
+              rows="3"
+              id="contactMessage"
+              name="contactMessage"
+              placeholder="Your Message..."
+              required
+            />
+          </div>
+         
+          <input type="file" name="file" onChange={this.onChange} /> 
+          <button
+            className="btn btn-primary btn-block"
+            type="submit">
+            Invio 
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+  }
 
 }
 
